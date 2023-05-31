c595e765560ccb65fb84441b192e91533414cfce
SysSimone <developer.desenv@gmail.com>
2023-05-30 23:28:21 -0300

    Criando funcionalidades

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,41 +1,26 @@
-import { FaRocket } from 'react-icons/fa';
-
+import { SiLinkedin } from 'react-icons/si';
+import { AiFillInstagram, AiFillGithub } from 'react-icons/ai'
 import '../Rodape';
 
-const Rodape = () => {
-  //const instagramUrl = 'https://www.instagram.com/sy.balzan';
-  //const linkedinUrl = 'https://www.linkedin.com/in/simonebsdev';
-  //const twitterUrl = 'https://twitter.com/sybalzan';
-  //const githubUrl = 'https://github.com/SysSimone';
-  
-  return (
-    
-<footer className="rodape">
-        
-      <section>    
-        
-        <ul>
-          <li>      
-          
-          <a href="https://www.linkedin.com/in/simonebsdev/"><FaRocket /> Clique aqui para visualizar meu Linkedin
-              
-            </a>
-          </li>
-          <li>
-          <a href="https://www.instagram.com/sy.balzan/">
 
-              Clique aqui para visualizar meu instagram
-             </a>
-          </li>
-          <li>
-          <a href="https://github.com/SysSimone">
-              Clique aqui para visualizar meu GitHub
-            </a>
-          </li>
-          
-        </ul>
-      </section>
-    </footer>
+const Rodape = () => {
+   return (   
+      <ul className='icons-container' >
+        <div >
+          <a href="https://www.linkedin.com/in/simonebsdev/"> <SiLinkedin color={'black'} size={30}  />
+          </a>
+        </div>
+        <div >
+          <a href="https://www.instagram.com/sy.balzan/"><AiFillInstagram color={'black'} size={30} display={'flex'} flex-direction={'column'} />
+          </a>
+        </div>
+        <div >
+          <a href="https://github.com/SysSimone/profile"><AiFillGithub color={'black'} size={30} display={'flex'} flex-direction={'column'} />
+          </a>
+        </div>
+      </ul>
+
+    
   );
 }
 
