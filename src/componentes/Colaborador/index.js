bbe1e9708aa51708b81b69aac264bba5b166f424
SysSimone <developer.desenv@gmail.com>
2023-05-31 11:43:15 -0300

    refatorando camponentes

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,8 +1,17 @@
-import {AiFillCloseCircle} from 'react-icons/ai';
+import {AiFillCloseCircle, AiFillHeart, AiOutlineHeart} from 'react-icons/ai';
 import './Colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
-    return (<div className="colaborador">
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar(){
+        aoFavoritar(colaborador.id);
+    }
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
+    return (
+    <div className="colaborador">
         <AiFillCloseCircle 
         size={25} 
         className="deletar"
@@ -15,8 +24,15 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito 
+                ? <AiFillHeart {...propsfavorito} color="#ff0000"/> 
+                : <AiOutlineHeart {...propsfavorito}/>
+                }
+            </div>
         </div>
-    </div>)
+    </div>
+    )
 }
 
 export default Colaborador;
