dec46596b748d5beb90f1884d4fb496e7066a216
SysSimone <developer.desenv@gmail.com>
2023-06-04 19:03:24 -0300

    rodapé personalizado

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,46 +1,41 @@
-// import { SiLinkedin } from 'react-icons/si';
-// import { AiFillInstagram, AiFillGithub } from 'react-icons/ai'
-import '../Rodape';
-
+import {AiFillGithub, AiFillLinkedin, AiFillInstagram} from 'react-icons/ai';
+import './Rodape.css'
 
 const Rodape = () => {
-  const instagramUrl = 'https://www.instagram.com/sy.balzan';
+    const instagramUrl = 'https://www.instagram.com/sy.balzan';
+    const linkedinUrl = "https://www.linkedin.com/in/simonebsdev/";
+  const githubUrl = "https://github.com/SysSimone";
 
-  return (<footer className="footer">
-     <section>
-     <ul>
-          <li>
-               <a href={instagramUrl} target="_blank" rel="noopener noreferrer">
-                  <img src="/imagens/instagram.png" alt="logo instagram" />
-                  
-              </a>
-          </li>
-          <li>
-              <a href="twitter.com" target="_blank">
-                  <img src="/imagens/twitter.png" alt="" />
-              </a>
-          </li>
-          <li>
-              <a href="instagram.com" target="_blank">
-                  <img src="/imagens/instagram.png" alt="" />
-              </a>
-          </li>
-      </ul>
-     </section>
-     <section>
-     <img src="/imagens/logo.png" alt="" />
-     </section>
-     <section>
-      <p>
-          Desenvolvido por Alura.
-      </p>
-     </section>
-  </footer>)
+    return (<footer className="footer">
+       <section>
+       <ul>
+            <li>
+                 <a href={instagramUrl} target="_blank" rel="noopener noreferrer">
+                 <AiFillInstagram size={50}/>
+                    
+                </a>
+            </li>
+            <li>
+                <a href={linkedinUrl}  target="_blank"  rel="noopener noreferrer">
+                <AiFillLinkedin size={50}/>
+                </a>
+            </li>
+            <li>
+                <a href={githubUrl} target="_blank"  rel="noopener noreferrer">
+                <AiFillGithub size={50}/>
+                </a>
+            </li>
+        </ul>
+       </section>
+       <section>
+       
+       </section>
+       <section>
+        <p>
+       Desenvolvido por Simonebsdev. Acessse minhas redes sociais!
+        </p>
+       </section>
+    </footer>)
 }
 
 export default Rodape
-
-
-
-
-
